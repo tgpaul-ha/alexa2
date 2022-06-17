ac3e0bae9f02c6533e581526306009c377658f85
tgpaul-ha <53988861+tgpaul-ha@users.noreply.github.com>
2022-06-16 20:57:07 -0400

    Update capabilities.py

--- custom_components/alexa2/capabilities.py
+++ custom_components/alexa2/capabilities.py
@@ -439,7 +439,7 @@ class AlexaCooking(AlexaCapability):
         """
         configuration = {}
         configuration["supportsRemoteStart"] = True
-        configuration["supportedCookingModes"] = [{"value": "REHEAT"}]
+        configuration["supportedCookingModes"] = [{"cookingMode": "REHEAT"}]
 
         return configuration
     
